e03f5e030d5a7fc6668151ef3b7685c3ba294456
taram <tsayouba@gmail.com>
2014-12-20 00:07:43 +0000

    update files

--- src/main/java/com/neo/beans/AuthBean.java
+++ src/main/java/com/neo/beans/AuthBean.java
@@ -17,32 +17,35 @@ public class AuthBean {
 	private String username;
 	private String password;
 	private boolean remember=false;
-	
+
 	/*
 	 * connexion de l'utilisateur
 	 */
 	public String login() {
+		System.out.println("appel de la page");
 		desable=true;
 		Subject currentUser=SecurityUtils.getSubject();
 		UsernamePasswordToken token=new UsernamePasswordToken(username, password,remember);
-			try {
-				currentUser.login(token);
-			} catch (AuthenticationException e) {
-				FacesContext.getCurrentInstance().addMessage(null,
-						new FacesMessage("Veillez saisir des idenfiants corrects."));
-				desable=false;
-				return "pretty:login";
-			}
+		try {
+			currentUser.login(token);
+		} catch (AuthenticationException e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Veillez saisir des idenfiants corrects."));
+			desable=false;
+			return "pretty:login";
+		}
 		return "pretty:userhome";
 	}
 	
-	
+	public void verifyLogin() {
+		System.out.println("Appel de methode");
+	}
+
 	/*
 	 * deconnection de l'utilisateur
 	 */
 	public String  logout() {
 		Subject currentUser=SecurityUtils.getSubject();
-		
 		try {
 			currentUser.logout();
 			desable=false;
@@ -50,30 +53,29 @@ public class AuthBean {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return null;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public boolean isRemember() {
 		return remember;
 	}
-	
+
 	public void setRemember(boolean remember) {
 		this.remember = remember;
 	}
