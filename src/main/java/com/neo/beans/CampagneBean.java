feba0f68c617be3eecffc518bcec91b9544e8fe2
Mystic <Mystic@192.168.1.14>
2014-12-21 18:52:18 +0000

    yes

--- src/main/java/com/neo/beans/CampagneBean.java
+++ src/main/java/com/neo/beans/CampagneBean.java
@@ -1,9 +1,11 @@
 package com.neo.beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.servlet.http.Part;
 
 import com.neo.dao.CampagneDAO;
 import com.neo.domaine.Campagne;
@@ -14,23 +16,25 @@ import com.neo.domaine.Publicite;
 public class CampagneBean {
 
 	private boolean showPub=false;
+	private Part fichier;
 	private CampagneDAO campagneDAO;
 	private Campagne campagne;
 	private Publicite publicite;
 	private List<Publicite> lesPublicites;
-	
-	
+	private List<Campagne> lesCampagnes;
+
+
 	public CampagneBean() {
 		campagne=new Campagne();
 		publicite=new Publicite();
+		lesPublicites=new ArrayList<Publicite>();
+		lesCampagnes=new ArrayList<Campagne>();
 	}
-	
-	/**
-	 * 
-	 * affichage du formulaire des pubs
-	 */
+
+
+	// affichage du formulaire des pubs
 	public String afficherPub() {
-	
+
 		if(showPub){
 			setShowPub(false);
 		}
@@ -40,10 +44,18 @@ public class CampagneBean {
 		System.out.println("Essaiu");
 		return null;
 	}
-	
-	
-	
-	
+
+	//ajout de la publicite
+	public void addPublicite(){
+     System.out.println("ds addpub");
+     
+	}
+   
+	//ajout de la campagne
+	public void addCampagne(){
+		System.out.println("ds add campagne");
+	}
+
 	/**
 	 * 
 	 * Getters et Setters
@@ -89,8 +101,28 @@ public class CampagneBean {
 		this.lesPublicites = lesPublicites;
 	}
 
-	
-	
-	
+
+	public Part getFichier() {
+		return fichier;
+	}
+
+
+	public void setFichier(Part fichier) {
+		this.fichier = fichier;
+	}
+
+
+	public List<Campagne> getLesCampagnes() {
+		return lesCampagnes;
+	}
+
+
+	public void setLesCampagnes(List<Campagne> lesCampagnes) {
+		this.lesCampagnes = lesCampagnes;
+	}
+
+
+
+
 
 }
