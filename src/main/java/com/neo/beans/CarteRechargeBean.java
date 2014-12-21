afda944dcb5e3d820687362eb77e4a93e477a849
Mystic <Mystic@192.168.1.14>
2014-12-21 21:38:07 +0000

    added carte page et modif carte page et pub page

---
+++ src/main/java/com/neo/beans/CarteRechargeBean.java
@@ -0,0 +1,87 @@
+package com.neo.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.neo.dao.CarteDAO;
+import com.neo.daoImpl.CarteDaoImpl;
+import com.neo.domaine.Carte;
+
+@ManagedBean
+@SessionScoped
+public class CarteRechargeBean {
+	
+	private Carte carteRecharge;
+	private CarteDAO carteDAO;
+	private List<Carte> lesCartes;
+	
+	public CarteRechargeBean () {
+		carteRecharge=new Carte();
+		carteDAO=new CarteDaoImpl();
+		lesCartes=new ArrayList<Carte>();
+		lesCartes=carteDAO.lister();
+	}
+
+	// ajouter carte
+	public String addCarte(){
+		System.out.println("ds carte");
+		carteDAO.creer(carteRecharge);
+		carteRecharge=new Carte();
+		rafraichirListe();
+
+		return null;
+	}
+	
+	//modification de la carte
+	public String updateCarte(){
+		 carteDAO.modifier(carteRecharge);	
+		return null;
+	}
+	
+	
+	public String editCarte(Carte carte){
+		System.out.println("ds load");
+		return null;
+	}
+	
+	//raffraichir la liste des cartes
+	private  void rafraichirListe(){
+		lesCartes=carteDAO.lister();
+	}
+	
+	
+	/**
+	 * 
+	 * Getters et Setters
+	 */
+	
+	public CarteDAO getCarteDAO() {
+		return carteDAO;
+	}
+
+	public Carte getCarteRecharge() {
+		return carteRecharge;
+	}
+
+	public void setCarteRecharge(Carte carteRecharge) {
+		this.carteRecharge = carteRecharge;
+	}
+
+	public void setCarteDAO(CarteDAO carteDAO) {
+		this.carteDAO = carteDAO;
+	}
+
+	public List<Carte> getLesCartes() {
+		return lesCartes;
+	}
+
+	public void setLesCartes(List<Carte> lesCartes) {
+		this.lesCartes = lesCartes;
+	}
+
+	
+	
+}
