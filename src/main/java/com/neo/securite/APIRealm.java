ace1e3d942f3b98917eedcd8be3272a305ca5c50
taram <tsayouba@gmail.com>
2014-12-29 15:52:39 +0000

    added API security

---
+++ src/main/java/com/neo/securite/APIRealm.java
@@ -0,0 +1,52 @@
+package com.neo.securite;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import com.neo.dao.UtilisateurDAO;
+import com.neo.daoImpl.UtilisateurDAOImpl;
+import com.neo.domaine.Utilisateur;
+
+public class APIRealm extends AuthorizingRealm{
+	private UtilisateurDAO UtilisateurDAO;
+	
+	public APIRealm() {
+		setUtilisateurDAO(new UtilisateurDAOImpl());
+	}
+	
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(
+			PrincipalCollection principals) {
+		
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(
+			AuthenticationToken token) throws AuthenticationException {
+		
+		UsernamePasswordToken mToken=(UsernamePasswordToken) token;
+		
+		Utilisateur utilisateur=UtilisateurDAO.findByCompte(mToken.getUsername());
+		if(utilisateur==null){
+			throw new AuthenticationException("Ce compte n'existe pas");
+		}
+		SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(utilisateur.getCompte(), utilisateur.getMotDePasse(), getName());
+		return info;
+	}
+
+	public UtilisateurDAO getUtilisateurDAO() {
+		return UtilisateurDAO;
+	}
+
+	public void setUtilisateurDAO(UtilisateurDAO utilisateurDAO) {
+		UtilisateurDAO = utilisateurDAO;
+	}
+
+}
